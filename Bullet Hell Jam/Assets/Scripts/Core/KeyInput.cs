17fe3c8627c11e4165909e3468be362596b0fd0d
GammaFoxTBG <67077419+GammaFoxTBG@users.noreply.github.com>
2021-04-14 10:50:24 -0600

    Added Basic Player/Input Controller
    I was bored and the jam said pre-made scripts like Player Controllers are okay.

---
+++ Bullet Hell Jam/Assets/Scripts/Core/KeyInput.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public struct KeyInput
+{
+    public Vector3 moveVec;
+    public bool shootPress;
+    public bool crawlPress;
+}
---
+++ Bullet Hell Jam/Assets/Scripts/Overhead/InputController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public KeyInput keyInput;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        keyInput.moveVec = new Vector3(moveX, moveY, 0f);
+        keyInput.crawlPress = Input.GetKey(KeyCode.LeftShift);
+    }
+}
