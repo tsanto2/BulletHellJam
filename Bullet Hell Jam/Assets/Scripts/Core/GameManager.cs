179b273aeced11c262b6e0b9f4cd2beaa731a11e
GammaFoxTBG <67077419+GammaFoxTBG@users.noreply.github.com>
2021-04-16 10:36:49 -0600

    Added Slowmo UI
    Small circle to indicate when you can use slowmo. As soon as you release the slowmo button or you pass the max time, the timer starts and the circle fills while it's on cooldown.

--- Bullet Hell Jam/Assets/Scripts/Core/GameManager.cs
+++ Bullet Hell Jam/Assets/Scripts/Core/GameManager.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
-using System;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,8 +12,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float slowmoScale;
     [SerializeField] private float slowmoDelay;
     [SerializeField] private float slowmoMaxTime;
+    [SerializeField] private Image slowlmoImage;
     private float slowmoCooldown;
     private float slowmoStartTime;
+    private float slowmoStopTime;
     private bool isSlowmoActive;
 
     private WaitForSeconds tenSeconds = new WaitForSeconds(10f);
@@ -33,23 +36,33 @@ public class GameManager : MonoBehaviour
     private void HandleSlowmo()
     {
         if (input.keyInput.slowmoPress && Time.time >= slowmoCooldown)
-        {
-            Time.timeScale = slowmoScale;
-            slowmoStartTime = Time.time;
-            isSlowmoActive = true;
-            Debug.Log("Started slowmo at: " + Time.time);
-        }
+            StartSlowmo();
 
         if (!isSlowmoActive)
+        {
+            slowlmoImage.fillAmount = (Time.time - slowmoStopTime) / (slowmoCooldown - slowmoStopTime);
             return;
+        }
         
         if (input.keyInput.slowmoRelease || Time.time >= slowmoStartTime + slowmoMaxTime)
-        {
-            Time.timeScale = 1f;
-            slowmoCooldown = Time.time + slowmoDelay;
-            isSlowmoActive = false;
-            Debug.Log("Stopped slowmo at: " + Time.time);
-        }
+            StopSlowmo();
+    }
+
+    private void StartSlowmo()
+    {
+        Time.timeScale = slowmoScale;
+        slowmoStartTime = Time.time;
+        isSlowmoActive = true;
+        Debug.Log("Started slowmo at: " + Time.time);
+    }
+
+    private void StopSlowmo()
+    {
+        Time.timeScale = 1f;
+        slowmoCooldown = Time.time + slowmoDelay;
+        isSlowmoActive = false;
+        slowmoStopTime = Time.time;
+        Debug.Log("Stopped slowmo at: " + Time.time);
     }
 
     IEnumerator TenSecondTimer()
