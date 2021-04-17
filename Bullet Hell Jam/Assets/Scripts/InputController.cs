a0000b071027498677d4429a26e5ca5c01bee6a4
tsanto2 <tsanto2.ts@gmail.com>
2021-04-16 20:48:34 -0600

    Added controller support.

--- Bullet Hell Jam/Assets/Scripts/InputController.cs
+++ Bullet Hell Jam/Assets/Scripts/InputController.cs
@@ -4,18 +4,32 @@ public class InputController : MonoBehaviour
 {
     public KeyInput keyInput;
 
+    private bool RTActive = false;
+
     private void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         keyInput.moveVec = new Vector3(moveX, moveY, 0f);
-        keyInput.crawlPress = Input.GetKey(KeyCode.LeftShift);
+        keyInput.crawlPress = Input.GetKey(KeyCode.LeftShift) || (Input.GetAxisRaw("Crawl") > 0);
 
-        keyInput.shootPress = Input.GetKey(KeyCode.Space);
+        keyInput.shootPress = Input.GetButton("Shoot");
 
-        keyInput.slowmoPress = Input.GetKeyDown(KeyCode.LeftControl);
-        keyInput.slowmoRelease = Input.GetKeyUp(KeyCode.LeftControl);
+        if (Input.GetAxisRaw("SlowMo") > 0)
+            RTActive = true;
+
+        keyInput.slowmoPress = Input.GetKeyDown(KeyCode.LeftControl) || RTActive;
+
+        bool shouldReleaseRT = false;
+        if (RTActive && Input.GetAxisRaw("SlowMo") < 1)
+        {
+            shouldReleaseRT = true;
+            RTActive = false;
+        }
+
+        keyInput.slowmoRelease = Input.GetKeyUp(KeyCode.LeftControl) || shouldReleaseRT;
+
     }
 }
 
